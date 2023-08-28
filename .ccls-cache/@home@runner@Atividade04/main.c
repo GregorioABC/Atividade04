ce075ce4c9ef181893e66b544bf7c93eb1b051d5
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-08-28 11:55:25 +0000

    até a 6º questão

--- .ccls-cache/@home@runner@Atividade04/main.c
+++ .ccls-cache/@home@runner@Atividade04/main.c
@@ -1,18 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-  int num1;
-  int num2;
-  printf("Informe o 1º número ");
-  scanf("%d",&num1);
-  printf("Informe o 2º número ");
-  scanf("%d",&num2);
-  if(num1>num2){
-    printf("O número 1 é maior que o numero 2 ");
-  }else if(num2>num1){
-    printf("O número 2 é maior que o numero 1 ");
-  }else{
-    printf("Os números são iguais ");
-  }
-  return 0;
-}
+// Repostas:
+// 1.
+// 2.
+// 3.
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void) {
+//   int numero;
+//   printf("Informe um numero : ");
+//   scanf("%d",&numero);
+//   if(numero>0){
+//     printf("O número é positivo");
+//   }else if(numero == 0){
+//     printf("O número é igual a 0 ");
+    
+//   }else{
+//     printf("O número é negativo");
+//   }
+//   return 0;
+// }
+// 4.
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void) {
+//   int idade;
+//   printf("Informe a sua idade : ");
+//   scanf("%d",&idade);
+//   if (idade < 18){
+//     printf("Você é menor de idade");
+//   }else{
+//     printf("Você é maior de idade ");
+//   }
+//   return 0;
+// }
+// 5.
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void) {
+//   int num1;
+//   int num2;
+//   printf("Informe o 1º número ");
+//   scanf("%d",&num1);
+//   printf("Informe o 2º número ");
+//   scanf("%d",&num2);
+//   if(num1>num2){
+//     printf("O número 1 é maior que o numero 2 ");
+//   }else if(num2>num1){
+//     printf("O número 2 é maior que o numero 1 ");
+//   }else{
+//     printf("Os números são iguais ");
+//   }
+//   return 0;
+// }
+//6.
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void) {
+//   int num1;
+//   int num2;
+//   printf("Informe o 1º número ");
+//   scanf("%d",&num1);
+//   printf("Informe o 2º número ");
+//   scanf("%d",&num2);
+//   if(num1>num2){
+//     printf("O número 1 é maior que o numero 2 ");
+//   }else if(num2>num1){
+//     printf("O número 2 é maior que o numero 1 ");
+//   }else{
+//     printf("Os números são iguais ");
+//   }
+//   return 0;
+// }
